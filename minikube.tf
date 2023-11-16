cd12537db9c4028a97a85a140c2fa23f9b8fb9be
srivijay1 <srivijay12071@gmail.com>
2023-11-16 11:30:46 +0530

    minikube

---
+++ minikube.tf
@@ -0,0 +1,22 @@
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region    = "us-east-1"
+  cluster_name  = "Robo"
+  aws_instance_type = "t3.medium"
+  ssh_public_key = "~/terraform.pub"
+  aws_subnet_id = "subnet-0e79aa1def89cb336" # any public subnet in any VPC, I am using default VPC subnet
+  hosted_zone = "srivijay.online" # replace with your domain
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
