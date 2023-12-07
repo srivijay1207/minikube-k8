f0dab4548798d8a70a5d9b99d853eca4cd6da176
srivijay1 <srivijay12071@gmail.com>
2023-12-07 12:23:50 +0530

    minikube

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.15.0"
+    }
+  }
+   backend "s3" {
+    bucket   = "rs-remote-state-dev"
+    key = "minikube"
+    region = "us-east-1"
+    dynamodb_table = "dynamodb-table-dev"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
